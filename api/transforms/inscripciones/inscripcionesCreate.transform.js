4a791c1e7f16f193ba2a3eeac64b36f0edab2445
MarcosC-14 <marcoscarab@hotmail.com>
2026-05-30 21:02:43 -0300

    feat: Inscripciones create DTO
    Ahora se utiliza un dto de entrada para evitar usar req.body innecesariamente

--- api/transforms/inscripciones/inscripcionesCreate.transform.js
+++ api/transforms/inscripciones/inscripcionesCreate.transform.js
@@ -1,11 +1,17 @@
+import InscripcionesCreateDTO from "../../dtos/inscripciones/inscripcionesCreate.dto.js";
+
 const inscripcionesCreateTransform = (req, res, next) => {
     const { idCurso, idEstudiante, idUsuarioModificacion } = req.body;
 
-    req.data = {
-        idCurso,
-        idEstudiante,
-        idUsuarioModificacion
-    };
+    const idCursoDTO = Number(idCurso);
+    const idEstudianteDTO = Number(idEstudiante);
+    const idUsuarioModificacionDTO = Number(idUsuarioModificacion);
+    
+    req.dto = new InscripcionesCreateDTO (
+        idCursoDTO,
+        idEstudianteDTO,
+        idUsuarioModificacionDTO
+    );
 
     next();
 };
